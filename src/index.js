a89d05e1a0aadfe410a7bce69a03c10413e832d9
Alexandr Axenov (axenov_it) <axenov.its@gmail.com>
2022-06-07 20:10:36 +0300

    Start project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { slideLeft, slideRight } from "./slider";
+
+// slider();
+
+console.log(slideLeft());
+console.log(slideRight());
---
+++ src/slider.js
@@ -0,0 +1,17 @@
+function slider() {
+  console.log("hello sliderssss");
+  console.log("hello sliderssss");
+  console.log("hello sliderssss");
+  console.log("hello sliderssss");
+}
+
+export function slideLeft() {
+  return "hello slider left";
+}
+
+export function slideRight() {
+  slider();
+  return "hello slider right";
+}
+
+export default slider;
